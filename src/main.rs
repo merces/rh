591c53475a0f350df0252e7977307b6209e88f7d
merces <nandu88@gmail.com>
2025-07-27 15:41:57 -0300

    Better error handling

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,23 @@ use std::fs::File;
 use std::io::Read;
 use std::process::exit;
 
-#[cfg(test)]
-mod tests;
-
 const CHUNK_SIZE: usize = 4096; // 4KB
 const SEPARATOR_DWORD: char = ' ';
 const NON_PRINTABLE_CHAR: char = ' ';
 
-fn dump_line(data: &[u8], offset: usize) -> String {
-    assert!(data.len() <= 16);
+fn dump_line(data: &[u8], offset: usize) -> Result<String, &'static str> {
+    let data_len = data.len();
+
+    if data_len > 16 {
+        return Err("Warning: received more than 16 bytes of data. Only 16 bytes will be taken.");
+    }
 
     let mut line = String::with_capacity(76);
     let mut ascii = String::with_capacity(16);
 
     line.push_str(&format!("{offset:08X}:"));
 
-    for (i, byte) in data.iter().enumerate() {
+    for (i, byte) in data.iter().take(16).enumerate() {
         if i > 0 && i % 4 == 0 {
             line.push(SEPARATOR_DWORD);
         } else {
@@ -29,7 +30,7 @@ fn dump_line(data: &[u8], offset: usize) -> String {
 
         // handle ASCII
         let c = *byte as char;
-        if c.is_ascii_graphic() {
+        if c.is_ascii_graphic() || c == ' ' {
             ascii.push(c);
         } else {
             ascii.push(NON_PRINTABLE_CHAR);
@@ -38,14 +39,14 @@ fn dump_line(data: &[u8], offset: usize) -> String {
 
     line = format!("{:59}", line);
     line.push_str(&ascii);
-    line
+    Ok(line)
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 2 {
-        println!("I need a file to work with. 🥹");
+        eprintln!("I need a file to work with. 🥹");
         exit(1);
     }
 
@@ -71,7 +72,10 @@ fn main() {
         let mut pos = 0;
         while pos < bytes_read {
             let end = (pos + 16).min(bytes_read); // it can't exceed `bytes_read`
-            println!("{}", dump_line(&buffer[pos..end], ofs + pos));
+            match dump_line(&buffer[pos..end], ofs + pos) {
+                Ok(line) => println!("{line}"),
+                Err(err) => eprintln!("{err}"),
+            };
             pos += 16;
         }
 
@@ -80,4 +84,79 @@ fn main() {
 
     // useful to quickly know the file size :)
     println!("{ofs:08x}:");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn hexdump_one() {
+        let data: [u8; 1] = [0x90];
+        assert_eq!(
+            dump_line(&data, 0),
+            Ok(format!(
+                "00000000: 90                                               {NON_PRINTABLE_CHAR}"
+            ))
+        );
+    }
+    #[test]
+    fn hexdump_a() {
+        let data: [u8; 1] = ['A' as u8];
+        assert_eq!(
+            dump_line(&data, 0),
+            Ok(format!(
+                "00000000: 41                                               A"
+            ))
+        );
+    }
+    #[test]
+    fn hexdump_one_zero() {
+        let data: [u8; 1] = [0u8];
+        assert_eq!(
+            dump_line(&data, 0),
+            Ok(format!(
+                "00000000: 00                                               {NON_PRINTABLE_CHAR}"
+            ))
+        );
+    }
+    #[test]
+    fn hexdump_four() {
+        let data: &[u8] = "paix".as_bytes();
+        assert_eq!(
+            dump_line(&data, 0),
+            Ok(format!(
+                "00000000: 70 61 69 78                                      paix"
+            ))
+        );
+    }
+    #[test]
+    fn hexdump_five() {
+        let data: &[u8] = "paixx".as_bytes();
+        assert_eq!(
+            dump_line(&data, 0),
+            Ok(format!(
+                "00000000: 70 61 69 78{SEPARATOR_DWORD}78                                   paixx"
+            ))
+        );
+    }
+    #[test]
+    fn hexdump_eight() {
+        let data: &[u8] = "fernando".as_bytes();
+        assert_eq!(
+            dump_line(data, 0),
+            Ok(format!(
+                "00000000: 66 65 72 6E{SEPARATOR_DWORD}61 6E 64 6F                          fernando"
+            ))
+        );
+    }
+    #[test]
+    fn hexdump_sixteen() {
+        let data: &[u8] = "1234567812345678".as_bytes();
+        assert_eq!(
+            dump_line(data, 0),
+            Ok(format!(
+                "00000000: 31 32 33 34{SEPARATOR_DWORD}35 36 37 38{SEPARATOR_DWORD}31 32 33 34{SEPARATOR_DWORD}35 36 37 38  1234567812345678"
+            ))
+        );
+    }
 }
